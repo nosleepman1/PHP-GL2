781adef51978ad968392fde5d64d5dbc7f53a32b
nosleepman1 <abdallahdiouf.dev@gmail.com>
2026-03-04 08:18:14 +0000

    Fix devoir_PHP tracking

---
+++ devoir_PHP/database/products_db.php
@@ -0,0 +1,64 @@
+<?php 
+
+    require 'db_connection.php';
+
+
+
+    function create($libelle, $prix, $quantite, $description) {
+        global $pdo;
+
+        $sql = "INSERT INTO products (libelle, prix, quantite, description) VALUES (:libelle, :prix, :quantite, :description)";
+        $stmt = $pdo->prepare($sql);
+        return $stmt->execute(
+            [
+                'libelle' => $libelle,
+                'prix' => $prix,
+                'quantite' => $quantite,
+                ':description' => $description
+            ]
+        );
+    }
+
+    function update($id, $libelle, $prix, $quantite, $description) {
+        global $pdo;
+
+        $sql = "UPDATE products SET libelle = :libelle, prix = :prix, quantite = :quantite, description = :description WHERE id = :id";
+        $stmt = $pdo->prepare($sql);
+        return $stmt->execute(
+            [
+                'libelle' => $libelle,
+                'prix' => $prix,
+                'quantite' => $quantite,
+                ':description' => $description,
+                'id' => $id
+            ]
+        );
+    }
+
+    function delete($id) {
+        global $pdo;
+
+        $sql = "DELETE FROM products WHERE id = :id";
+        $stmt = $pdo->prepare($sql);
+        return $stmt->execute(['id' => $id]);
+    }
+
+
+
+    function getAll() {
+        global $pdo;
+
+        $sql = "SELECT * FROM products";
+        $stmt = $pdo->query($sql);
+        return $stmt->fetchAll();
+    }
+
+
+    function getById($id) {
+        global $pdo;
+
+        $sql = "SELECT * FROM products WHERE id = :id";
+        $stmt = $pdo->prepare($sql);
+        $stmt->execute(['id' => $id]);
+        return $stmt->fetch();
+    }
